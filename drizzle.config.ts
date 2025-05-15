27a1ffe573377070ebc1fae50a1aedd5b010620a
debghs <dghosh31428@gmail.com>
2025-05-16 00:24:14 +0530

    fix silly db-url bug

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,7 +7,7 @@ export default {
   out: './drizzle',
   driver: 'pg',
   dbCredentials: {
-    connectionString: process.env.NEON_DATABASE_URL!,
+    connectionString: process.env.DATABASE_URL!,
   },
   verbose: true,
   strict: true,
